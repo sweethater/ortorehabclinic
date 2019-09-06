642b0ac2ab9c04b045f91c1b6f9a20b9fa9e3d0b
Jakub Hadvig <jhadvig@redhat.com>
2019-09-06 13:00:08 +0200

    before nav rearrange

--- src/components/App.js
+++ src/components/App.js
@@ -19,15 +19,23 @@ const App = () => {
           <Route path="/new_stuff" component={Home}/>
         </div>
         <div className="navigation-container">
-          <NavLink className="nav" to="/"><NavItem title="PACIENTI"/></NavLink>
-          {/* <NavLink className="nav" to="/">PACIENTI</NavLink> */}
+          {/* <NavLink className="navigation-container--pacienti" to="/"><NavItem title="PACIENTI"/></NavLink>
+          <NavLink className="navigation-container--ortopedia" to="/">PACIENTI</NavLink>
           <NavLink className="nav" to="/stuff">ORTOPEDIA</NavLink>
           <NavLink className="nav" to="/new_stuff">FYZIOTERAPIA</NavLink>
           <NavLink className="nav" to="/stuff">REHABILITACIE</NavLink>
           <NavLink className="nav" to="/new_stuff">DENZITOMETRIA</NavLink>
           <NavLink className="nav" to="/stuff">PRISTROJE</NavLink>
           <NavLink className="nav" to="/new_stuff">CENNIK</NavLink>
-          <NavLink className="nav" to="/stuff">KONTAKT</NavLink>
+          <NavLink className="nav" to="/stuff">KONTAKT</NavLink> */}
+
+          <NavLink className="navigation-item" to="/"><NavItem title="PACIENTI" /></NavLink>
+          <NavLink className="navigation-item" to="/"><NavItem title="ORTOPEDIA" /></NavLink>
+          <NavLink className="navigation-item" to="/stuff"><NavItem title="FYZIOTERAPIA" /></NavLink>
+          <NavLink className="navigation-item" to="/new_stuff"><NavItem title="REHABILITACIA" /></NavLink>
+          <NavLink className="navigation-item" to="/stuff"><NavItem title="DENZITOMETRIA" /></NavLink>
+          <NavLink className="navigation-item" to="/new_stuff"><NavItem title="POPLATKY" /></NavLink>
+          <NavLink className="navigation-item" to="/stuff"><NavItem title="KONTAKT" /></NavLink>
         </div>
       </div>
     </HashRouter>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import fyzioterapia from '../assets/fyzioterapia.jpg';
+// import fyzioterapia from '../assets/nav-fyzio.png';
 // import patientInfo from '../assets/patientInfo2.jpg';
 
 import './Nav.css';
 
 export const NavItem = ({imgName, title}) => {
-  return <div>
+  return <div className="co-navigation__wrapper">
     {/* <img className="co-navigation__image" src={fyzioterapia}/> */}
+    {/* <div className="co-navigation__image" ></div> */}
     <span className="co-navigation__title">{title}</span>
   </div>;
 } 
