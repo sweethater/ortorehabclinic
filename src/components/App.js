dd969714b425993e64679b0444780b345b857886
Jakub Hadvig <jhadvig@redhat.com>
2019-09-07 16:55:38 +0200

    Add navigation components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
 
-import Home from "./Home";
+import { Home, Orthopedic, Patient, Fyzio, Rehab, Denzito, Gallery, Fees, Contact } from "./Home";
 import Stuff from "./Stuff";
 import { NavItem, NavHeader } from "./Nav";
 import back from '../assets/back2.png';
@@ -14,20 +14,27 @@ const App = () => {
       <div className="app-container">
         <div className="side-container">
           <NavLink className="navigation-item--back" to="/"><img src={back} /></NavLink>
+
           <NavLink className="navigation-item" to="/"><NavHeader /></NavLink>
-          <NavLink className="navigation-item" to="/"><NavItem title="Pacienti" index={1} /></NavLink>
-          <NavLink className="navigation-item" to="/"><NavItem title="Ortopédia" index={2} /></NavLink>
-          <NavLink className="navigation-item" to="/stuff"><NavItem title="Fyzioterapia" index={3} /></NavLink>
-          <NavLink className="navigation-item" to="/new_stuff"><NavItem title="Rehabilitácia" index={4} /></NavLink>
-          <NavLink className="navigation-item" to="/stuff"><NavItem title="Denzitometria" index={5} /></NavLink>
-          <NavLink className="navigation-item" to="/new_stuff"><NavItem title="Galéria" index={6} /></NavLink>
-          <NavLink className="navigation-item" to="/new_stuff"><NavItem title="Poplatky" index={7} /></NavLink>
-          <NavLink className="navigation-item" to="/stuff"><NavItem title="Kontakt" index={8} /></NavLink>
+          <NavLink className="navigation-item" to="/patient"><NavItem title="Pacienti" index={1} /></NavLink>
+          <NavLink className="navigation-item" to="/orthopedic"><NavItem title="Ortopédia" index={2} /></NavLink>
+          <NavLink className="navigation-item" to="/fyzio"><NavItem title="Fyzioterapia" index={3} /></NavLink>
+          <NavLink className="navigation-item" to="/rehab"><NavItem title="Rehabilitácia" index={4} /></NavLink>
+          <NavLink className="navigation-item" to="/denzito"><NavItem title="Denzitometria" index={5} /></NavLink>
+          <NavLink className="navigation-item" to="/gallery"><NavItem title="Galéria" index={6} /></NavLink>
+          <NavLink className="navigation-item" to="/fees"><NavItem title="Poplatky" index={7} /></NavLink>
+          <NavLink className="navigation-item" to="/contact"><NavItem title="Kontakt" index={8} /></NavLink>
         </div>
         <div className="main-container">
           <Route exact path="/" component={Home}/>
-          <Route path="/stuff" component={Stuff}/>
-          <Route path="/new_stuff" component={Home}/>
+          <Route path="/patient" component={Patient}/>
+          <Route path="/orthopedic" component={Orthopedic}/>
+          <Route path="/fyzio" component={Fyzio}/>
+          <Route path="/rehab" component={Rehab}/>
+          <Route path="/denzito" component={Denzito}/>
+          <Route path="/gallery" component={Gallery}/>
+          <Route path="/fees" component={Fees}/>
+          <Route path="/contact" component={Contact}/>
         </div>
       </div>
     </HashRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,29 +9,89 @@ import smSystem from '../assets//carousel/sm-system.jpg';
 import trTherapy from '../assets//carousel/tr-therapy.jpg';
 import './Home.css';
  
-class Home extends Component {
-  render() {
-    return (
-      <div className="co-home-container">
-        <img className="co-home-container__logo-image" src={logo}/>
-        <p className="co-home-container__paragraph">ORTO REHAB CLINIC je dynamicky sa rozvíjajúcim moderným zdravotníckym pracoviskom, ktoré poskytuje svojim pacientom vysoko kvalifikovanú lekársku starostlivosť v špecializovanom odbore ortopédia, denzitometria, cievna chirurgia a fyzioterapia.</p>
-        <p className="co-home-container__paragraph">ORTO REHAB CLINIC disponuje najmodernejšou zdravotníckou technikou v segmente ambulantnej zdravotnej starostlivosti, a to nielen v oblasti ortopédie, denzitometrie, ale aj fyzioterapie.</p>
-        <p className="co-home-container__paragraph">Hlavnou devízou ORTO REHAB CLINIC je najmä efektívne objednávanie pacientov a minimálne čakacie doby, čo nám umožňuje rýchlo definovať a identifikovať zdravotný problém pacienta.</p>
-        <div className="co-home-container__carousel">
-          <Carousel centered
-            infinite
-            arrows
-            itemWidth={1100}
-            offset={20}
-            slidesPerPage={2}>
-            <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={detskyUltrazvuk} href="#"/></NavLink>
-            <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={smSystem} href="#"/></NavLink>
-            <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={trTherapy} href="#"/></NavLink>
-          </Carousel>
-        </div>
+export const Home = props => {
+  return (
+    <div className="co-home-container co-contant-container">
+      <img className="co-home-container__logo-image" src={logo}/>
+      <p className="co-home-container__paragraph">ORTO REHAB CLINIC je dynamicky sa rozvíjajúcim moderným zdravotníckym pracoviskom, ktoré poskytuje svojim pacientom vysoko kvalifikovanú lekársku starostlivosť v špecializovanom odbore ortopédia, denzitometria, cievna chirurgia a fyzioterapia.</p>
+      <p className="co-home-container__paragraph">ORTO REHAB CLINIC disponuje najmodernejšou zdravotníckou technikou v segmente ambulantnej zdravotnej starostlivosti, a to nielen v oblasti ortopédie, denzitometrie, ale aj fyzioterapie.</p>
+      <p className="co-home-container__paragraph">Hlavnou devízou ORTO REHAB CLINIC je najmä efektívne objednávanie pacientov a minimálne čakacie doby, čo nám umožňuje rýchlo definovať a identifikovať zdravotný problém pacienta.</p>
+      <div className="co-home-container__carousel">
+        <Carousel centered
+          infinite
+          arrows
+          itemWidth={1100}
+          offset={20}
+          slidesPerPage={2}>
+          <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={detskyUltrazvuk} href="#"/></NavLink>
+          <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={smSystem} href="#"/></NavLink>
+          <NavLink className="nav" to="/stuff"><img className="co-home-container__carousel-image" src={trTherapy} href="#"/></NavLink>
+        </Carousel>
       </div>
-    );
-  }
+    </div>
+  );
 }
- 
-export default Home;
+
+export const Patient = props => {
+  return (
+    <div className="co-patient-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Orthopedic = props => {
+  return (
+    <div className="co-orthopedic-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Fyzio = props => {
+  return (
+    <div className="co-fyzio-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Rehab = props => {
+  return (
+    <div className="co-rehab-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Denzito = props => {
+  return (
+    <div className="co-denzito-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Gallery = props => {
+  return (
+    <div className="co-gallery-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Fees = props => {
+  return (
+    <div className="co-fees-container co-contant-container">
+
+    </div>
+  );
+}
+
+export const Contact = props => {
+  return (
+    <div className="co-contact-container co-contant-container">
+
+    </div>
+  );
+}
