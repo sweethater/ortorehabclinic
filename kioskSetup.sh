285c488cf077b7c9dd5071d5695c3e172968e66a
sweethater <htmtrade2@gmail.com>
2019-09-23 12:18:42 +0200

    Update startup.sh script

--- kioskSetup.sh
+++ kioskSetup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+sudo usermod -aG wheel kiosk
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install docker chromium chromium-libs-media-freeworld ffmpeg
 
 systemctl disable abrtd.service
+systemctl enable docker
+systemctl restart docker
+
+
+sudo groupadd docker
+sudo usermod -aG docker kiosk
+
+# sudo passwd -d kiosk
--- startup.sh
+++ startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-docer pull docker.io/jhadvig/ortorehabclinic-app:latest
+sleep 3 
+echo "test" > /tmp/startup.log
+amixer -q -D pulse sset Master 100%
 
-docker run -it -p 8080:80 docker.io/jhadvig/ortorehabclinic-app:latest
+docker pull docker.io/jhadvig/ortorehabclinic-app:latest
 
+docker run -d -p 8080:80 docker.io/jhadvig/ortorehabclinic-app:latest &
+
+sleep 3
+
+chromium-browser --kiosk --incognito --app=http://localhost:8080 --password-store=basic
