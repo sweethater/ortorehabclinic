ce76b6ed3b5d007f9d3b5b66faf4fd72aa1300d0
sweethater <htmtrade2@gmail.com>
2019-09-23 19:48:26 +0200

    scripts adjustments 2

--- cron-kiosk.sh
+++ cron-kiosk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-curl -s --head  https://raw.githubusercontent.com/sweethater/ortorehabclinic/master/startup_prehook_available.sh | head -n 1 | grep "HTTP/1.[01] [23].."
+curl -s --head  https://raw.githubusercontent.com/sweethater/ortorehabclinic/master/startup-prehook.sh | head -n 1 | grep "HTTP/1.[01] [23].."
 startup_prehook_available=$?
 
 if [ $startup_prehook_available -eq 0 ]
 then
-wget -q https://raw.githubusercontent.com/sweethater/ortorehabclinic/master/startup_prehook_available.sh -O /home/kiosk/startup_prehook_available.sh
-chmod +x /home/kiosk/startup_prehook_available.sh
+wget -q https://raw.githubusercontent.com/sweethater/ortorehabclinic/master/startup-prehook.sh -O /home/kiosk/startup-prehook.sh
+chmod +x /home/kiosk/startup-prehook.sh
 fi
 
 docker pull docker.io/jhadvig/ortorehabclinic-app:latest
